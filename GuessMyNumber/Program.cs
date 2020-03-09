f0f8b9fab58d72bd090db98776367070ade8271d
NoeRiveraJr <noe.rivera.jr@outlook.com>
2020-03-09 13:01:04 -0400

    Added a class that will contains the method that will conduct the bisection algorithm

--- GuessMyNumber/Program.cs
+++ GuessMyNumber/Program.cs
@@ -10,6 +10,16 @@ namespace GuessMyNumber
     {
         static void Main(string[] args)
         {
+            int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11};
+            BisectionAlgorithm bisection = new BisectionAlgorithm();
+            Console.WriteLine("Pick a number from 1 - 10 to see how long it takes the computer to guess that number!");
+            int check = Int32.Parse(Console.ReadLine());
+            Tuple<Array, string> bisect = (bisection.performBisection(list, check));
+            Console.WriteLine(bisect.Item2);
+            foreach(int x in bisect.Item1)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
